8d32155aa73fca5d6019f28c3b856e619e4d2348
bueno <matheusbartkev@gmail.com>
2024-08-27 04:22:39 -0300

    Correção da entidade para ser consumida

--- backend/src/app/Entities/Patient.entity.ts
+++ backend/src/app/Entities/Patient.entity.ts
@@ -1,8 +1,11 @@
+import { City } from "@prisma/client";
+
 export default interface Patient {
   id: string;
   name: string;
   birthDate: Date;
   email: string;
+  city: City;
   cityId: string;
   userId: string;
 
