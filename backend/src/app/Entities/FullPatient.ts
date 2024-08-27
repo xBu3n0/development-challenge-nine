dc36cc5b25dc697fdffbc0501f29c826705fe4f3
bueno <matheusbartkev@gmail.com>
2024-08-27 04:21:55 -0300

    Correção no schema

---
+++ backend/src/app/Entities/FullPatient.ts
@@ -0,0 +1,26 @@
+export interface FullPatient {
+  id: string;
+  name: string;
+  birthDate: Date;
+  email: string;
+  
+  city: {
+    id: string;
+    name: string;
+    stateId: string;
+    state: {
+      id: string;
+      name: string;
+      countryId: string;
+      country: {
+        id: string;
+        name: string;
+      }
+    }
+  };
+
+  cityId: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
